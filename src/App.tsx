ab4d2377f90dba54b40e77a5f8eddde8644b80e8
lee <lee88688@163.com>
2021-07-23 10:14:51 +0800

    feat: 更新软件包

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import TextField from '@material-ui/core/TextField';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  inputLine: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textFiled: {
+    flexGrow: 1
+  },
+  mode: {
+    display: 'flex',
+    justifyContent: 'center',
+  }
+});
+
+function App(): JSX.Element {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div className={classes.inputLine}>
+        <TextField className={classes.textFiled} variant="outlined"/>
+        <ArrowForwardIcon/>
+        <TextField className={classes.textFiled} variant="outlined"/>
+      </div>
+      <div className={classes.mode}>
+        <RadioGroup name="mode" row>
+          <FormControlLabel label="普通模式" value="normal" control={<Radio/>}/>
+          <FormControlLabel label="正则表达式" value="regex" control={<Radio/>}/>
+          <FormControlLabel label="增强模式" value="power" control={<Radio/>}/>
+        </RadioGroup>
+      </div>
     </div>
   );
 }
